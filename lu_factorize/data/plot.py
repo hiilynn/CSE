2b2fde79f9f15a5defc9464f55df0f91880ff9ce
axect <edeftg@gmail.com>
2019-04-01 14:43:19 +0900

    Prob 4-c)

--- lu_factorize/data/plot.py
+++ lu_factorize/data/plot.py
@@ -19,20 +19,40 @@ plt.rc('font', family='serif')
 #plt.savefig("prob_3b.png", dpi=300)
 
 # 3-c
-with open("prob_3c_1.pickle", "rb") as fr:
-    data_3c_1 = pickle.load(fr)
+#with open("prob_3c_1.pickle", "rb") as fr:
+#    data_3c_1 = pickle.load(fr)
+#
+#with open("prob_3c_2.pickle", "rb") as fr:
+#    data_3c_2 = pickle.load(fr)
+#
+#plt.figure(figsize=(10,6), dpi=300)
+#plt.plot(data_3c_1[0], data_3c_1[1], 'r.', label=r'Approx $(n=100)$', alpha=0.2)
+#plt.plot(data_3c_1[0], data_3c_1[2], 'r-', label=r'Exact $(n=100)$', alpha=0.2)
+#plt.plot(data_3c_2[0], data_3c_2[1], 'b.', label=r'Approx $(n=200)$', alpha=0.2)
+#plt.plot(data_3c_2[0], data_3c_2[2], 'b-', label=r'Exact $(n=200)$', alpha=0.2)
+#plt.title(r'Problem 3-c)', fontsize=16)
+#plt.xlabel(r'$x$', fontsize=14)
+#plt.ylabel(r'$y$', fontsize=14)
+#plt.grid()
+#plt.legend(fontsize=12)
+#plt.savefig("prob_3c.png", dpi=300)
 
-with open("prob_3c_2.pickle", "rb") as fr:
-    data_3c_2 = pickle.load(fr)
+# 4-c
+with open("prob_4c_1.pickle", "rb") as fr:
+    data_4c_1 = pickle.load(fr)
+
+with open("prob_4c_2.pickle", "rb") as fr:
+    data_4c_2 = pickle.load(fr)
 
 plt.figure(figsize=(10,6), dpi=300)
-plt.plot(data_3c_1[0], data_3c_1[1], 'r.', label=r'Approx $(n=100)$', alpha=0.2)
-plt.plot(data_3c_1[0], data_3c_1[2], 'r-', label=r'Exact $(n=100)$', alpha=0.2)
-plt.plot(data_3c_2[0], data_3c_2[1], 'b.', label=r'Approx $(n=200)$', alpha=0.2)
-plt.plot(data_3c_2[0], data_3c_2[2], 'b-', label=r'Exact $(n=200)$', alpha=0.2)
-plt.title(r'Problem 3-c)', fontsize=16)
+plt.plot(data_4c_1[0], data_4c_1[1], 'r.', label=r'Approx $(n=100)$', alpha=0.2)
+plt.plot(data_4c_1[0], data_4c_1[2], 'r-', label=r'Exact $(n=100)$', alpha=0.2)
+plt.plot(data_4c_2[0], data_4c_2[1], 'b.', label=r'Approx $(n=200)$', alpha=0.2)
+plt.plot(data_4c_2[0], data_4c_2[2], 'b-', label=r'Exact $(n=200)$', alpha=0.2)
+plt.title(r'Problem 4-c)', fontsize=16)
 plt.xlabel(r'$x$', fontsize=14)
 plt.ylabel(r'$y$', fontsize=14)
 plt.grid()
 plt.legend(fontsize=12)
-plt.savefig("prob_3c.png", dpi=300)
+plt.savefig("prob_4c.png", dpi=300)
+
